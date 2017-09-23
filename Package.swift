70773db5bea9e2cf6abeb00307e6a6ddb2728943
Alejandro Isaza <al@isaza.ca>
2017-09-22 22:38:32 -0700

    Use Swift tools version 4.0

--- Package.swift
+++ Package.swift
@@ -1,5 +1,23 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "Upsurge"
+    name: "Upsurge",
+    products: [
+        .library(
+            name: "Upsurge",
+            targets: ["Upsurge"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Upsurge",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "UpsurgeTests",
+            dependencies: ["Upsurge"]
+        ),
+    ]
 )
