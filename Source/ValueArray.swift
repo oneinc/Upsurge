59710146f8d7883d8e20098cd2084ea1411ba5ab
littleowl <tim@timothykautz.com>
2018-04-01 19:18:55 -0700

    ignore warnging for 9.2 support

--- Source/ValueArray.swift
+++ Source/ValueArray.swift
@@ -200,7 +200,7 @@ open class ValueArray<Element: Value>: MutableLinearType, ExpressibleByArrayLite
 
     deinit {
         if !unownedPointer {
-            mutablePointer.deallocate()
+            mutablePointer.deallocate(capacity: capacity)
         }
     }
 
